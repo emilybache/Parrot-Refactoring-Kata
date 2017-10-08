41f6a98e81d3110827111e981b44d2d7ee07bbc2
Attila Kerekes <attila.kerekes@erstegroup.com>
2017-10-08 15:47:31 +0200

    Port to JavaScript

---
+++ JavaScript/src/parrot.js
@@ -0,0 +1,38 @@
+export const PARROT_TYPES = {
+    EUROPEAN:       'EUROPEAN',
+    AFRICAN:        'AFRICAN',
+    NORWEGIAN_BLUE: 'NORWEGIAN_BLUE',
+};
+
+export class Parrot {
+    constructor(type, numberOfCoconuts, voltage, isNailed) {
+        this.type = type;
+        this.numberOfCoconuts = numberOfCoconuts;
+        this.voltage = voltage;
+        this.isNailed = isNailed;
+    }
+
+    getSpeed() {
+        switch (this.type) {
+            case PARROT_TYPES.EUROPEAN:
+                return this.getBaseSpeed();
+            case PARROT_TYPES.AFRICAN:
+                return Math.max(0, this.getBaseSpeed() - this.getLoadFactor() * this.numberOfCoconuts);
+            case PARROT_TYPES.NORWEGIAN_BLUE:
+                return (this.isNailed) ? 0 : this.getBaseSpeedWithVoltage(this.voltage);
+        }
+        throw new Error("Should be unreachable");
+    }
+
+    getBaseSpeedWithVoltage(voltage) {
+        return Math.min(24, voltage * this.getBaseSpeed());
+    }
+
+    getLoadFactor() {
+        return 9;
+    }
+
+    getBaseSpeed() {
+        return 12;
+    }
+}
