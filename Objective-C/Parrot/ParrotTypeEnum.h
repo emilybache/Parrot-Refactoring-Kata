fe0b8ca3d98a77298e2c6009f9f5e8b3792450ee
Ivan Rublev <ivan@ivanrublev.me>
2017-10-21 23:53:28 +0300

    Initial commit. Parrot class is ported.

---
+++ Objective-C/Parrot/ParrotTypeEnum.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, ParrotTypeEnum) {
+    ParrotTypeEnumEuropean,
+    ParrotTypeEnumAfrican,
+    ParrotTypeEnumNorwegianBlue,
+};
---
+++ Objective-C/ParrotTests/ParrotTests.m
@@ -0,0 +1,16 @@
+//
+//  ParrotTests.m
+//  ParrotTests
+//
+//  Created by Ivan Rublev on 10/21/17.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ParrotTests : XCTestCase
+@end
+
+@implementation ParrotTests
+
+
+@end
