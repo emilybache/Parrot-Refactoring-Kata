b50d496b9e3ab573a8805939ab98edef9372e013
Peter Kofler <peter.kofler@code-cop.org>
2017-11-04 17:06:20 +0100

    Format Parrot.

--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -3,10 +3,9 @@ package parrot;
 public class Parrot {
 
     private ParrotTypeEnum type;
-    private int numberOfCoconuts = 0;
+    private int numberOfCoconuts;
     private double voltage;
     private boolean isNailed;
-
 
     public Parrot(ParrotTypeEnum _type, int numberOfCoconuts, double voltage, boolean isNailed) {
         this.type = _type;
@@ -16,19 +15,20 @@ public class Parrot {
     }
 
     public double getSpeed() {
-        switch(type) {
+        switch (type) {
             case EUROPEAN:
                 return getBaseSpeed();
             case AFRICAN:
                 return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
             case NORWEGIAN_BLUE:
                 return (isNailed) ? 0 : getBaseSpeed(voltage);
+            default:
+                throw new RuntimeException("Should be unreachable");
         }
-        throw new RuntimeException("Should be unreachable");
     }
 
     private double getBaseSpeed(double voltage) {
-        return Math.min(24.0, voltage*getBaseSpeed());
+        return Math.min(24.0, voltage * getBaseSpeed());
     }
 
     private double getLoadFactor() {
@@ -38,6 +38,5 @@ public class Parrot {
     private double getBaseSpeed() {
         return 12.0;
     }
-
 
 }
