f56b763e574dab762f8b11fe28c33657b4a913ec
Peter Kofler <peter.kofler@code-cop.org>
2017-11-04 19:03:13 +0100

    Rename fields to private.

--- Python/parrot.py
+++ Python/parrot.py
@@ -10,21 +10,21 @@ class ParrotType(Enum):
 class Parrot:
 
     def __init__(self, type, number_of_coconuts, voltage, nailed):
-        self.type = type
-        self.number_of_coconuts = number_of_coconuts
-        self.voltage = voltage
-        self.nailed = nailed
+        self._type = type
+        self._number_of_coconuts = number_of_coconuts
+        self._voltage = voltage
+        self._nailed = nailed
 
     def speed(self):
-        if self.type == ParrotType.EUROPEAN:
+        if self._type == ParrotType.EUROPEAN:
             return self._base_speed()
-        if self.type == ParrotType.AFRICAN:
-            return max(0, self._base_speed() - self._load_factor() * self.number_of_coconuts)
-        if self.type == ParrotType.NORWEGIAN_BLUE:
-            if self.nailed:
+        if self._type == ParrotType.AFRICAN:
+            return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
+        if self._type == ParrotType.NORWEGIAN_BLUE:
+            if self._nailed:
                 return 0
             else:
-                return self._compute_base_speed_for_voltage(self.voltage)
+                return self._compute_base_speed_for_voltage(self._voltage)
 
         raise ValueError("should be unreachable")
 
