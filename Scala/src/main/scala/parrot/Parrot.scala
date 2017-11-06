67eecd8988d6efc577ef255042fcba83abf3b337
Peter Kofler <peter.kofler@code-cop.org>
2017-11-06 21:04:16 +0100

    Format again.

--- Scala/src/main/scala/parrot/Parrot.scala
+++ Scala/src/main/scala/parrot/Parrot.scala
@@ -2,30 +2,30 @@ package parrot
 
 import parrot.ParrotType._
 
-class Parrot (parrotType: ParrotType, numberOfCoconuts: Int, voltage: Double, isNailed: Boolean) {
-  
-    def getSpeed: Double = {
-      parrotType match {
-        case EUROPEAN =>
-          return getBaseSpeed
-        case AFRICAN =>
-          return Math.max(0, getBaseSpeed - getLoadFactor * numberOfCoconuts)
-        case NORWEGIAN_BLUE =>
-          return if ((isNailed)) 0 else getBaseSpeed(voltage)
-      }
-      throw new RuntimeException("Should be unreachable")
+class Parrot(parrotType: ParrotType, numberOfCoconuts: Int, voltage: Double, isNailed: Boolean) {
+
+  def getSpeed: Double = {
+    parrotType match {
+      case EUROPEAN =>
+        return getBaseSpeed
+      case AFRICAN =>
+        return Math.max(0, getBaseSpeed - getLoadFactor * numberOfCoconuts)
+      case NORWEGIAN_BLUE =>
+        return if ((isNailed)) 0 else getBaseSpeed(voltage)
     }
-  
-    private def getBaseSpeed(voltage: Double): Double = {
-      return Math.min(24.0, voltage * getBaseSpeed)
-    }
-  
-    private def getLoadFactor: Double = {
-      return 9.0
-    }
-  
-    private def getBaseSpeed: Double = {
-      return 12.0
-    }
+    throw new RuntimeException("Should be unreachable")
+  }
+
+  private def getBaseSpeed(voltage: Double): Double = {
+    return Math.min(24.0, voltage * getBaseSpeed)
+  }
+
+  private def getLoadFactor: Double = {
+    return 9.0
+  }
+
+  private def getBaseSpeed: Double = {
+    return 12.0
+  }
 
 }
