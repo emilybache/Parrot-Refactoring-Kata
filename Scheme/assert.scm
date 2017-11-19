828a69f5fb10141e4bb322076d3b34403d046ff7
Peter Kofler <peter.kofler@code-cop.org>
2017-11-20 00:15:48 +0100

    simplify usage of test framework, use lambdas instead of direct check

--- Scheme/assert.scm
+++ Scheme/assert.scm
@@ -4,61 +4,47 @@
 ;
 
 (define (report-error msg)
-    (error (string-append "AssertionError: " msg))
-)
+    (error (string-append "AssertionError: " msg)))
 
-(define (progress)
-    (display "."))
+(define (check msg b)
+    (if (not b) (report-error msg)))
 
 (define (assert msg b)
-    (if (not b) (report-error msg) (progress))
-)
+    (lambda () (check msg b)))
 
 ; extensions
 
 (define (assert-generic-equal to-string eq-op expected actual)
     (assert
         (string-append "expected:<" (to-string expected) "> but was:<" (to-string actual) ">")
-        (eq-op expected actual)
-    )
-)
+        (eq-op expected actual)))
 
 (define (assert= expected actual)
-    (assert-generic-equal number->string = expected actual)
-)
+    (assert-generic-equal number->string = expected actual))
 
 (define (assert-raise expected-exc body)
-    (with-exception-catcher
-        (lambda (exc)
-             (assert
-                (string-append "Should raise " (symbol->string expected-exc))
-                (eq? expected-exc exc)))
-        (lambda () (report-error (body)))))
+    (lambda ()
+        (with-exception-catcher
+            (lambda (exc)
+                 (check
+                    (string-append "Should raise " (symbol->string expected-exc))
+                    (eq? expected-exc exc)))
+            (lambda () (report-error (body))))))
 
-(define (test-case name assertions)
-    (define (exec asserts)
-        (if (not (= (length asserts) 0))
-            (append
-                (list ((car asserts)))
-                (exec (cdr asserts))
-            )
-            (list)
-        )
-
-    )
-    (display (string-append name " Test"))
+(define (test-case name assertion)
+    (display name)
     (newline)
-    (exec assertions)
+    (assertion)
+    (display "OK")
     (newline)
 )
 
 ; self test
 
-(test-case "Assert Self"
-    (list
-        (lambda () (assert= 1 1))
-        (lambda () (assert-raise
-            'a ; '
-            (lambda () (raise 'a)))) ; '))))
-    )
-)
+(test-case "assert="
+    (assert= 1 1))
+
+(test-case "assert-raise"
+    (assert-raise
+        'a ; '
+        (lambda () (raise 'a)))) ; '))))
--- Scheme/parrot-test.scm
+++ Scheme/parrot-test.scm
@@ -1,47 +1,35 @@
 (include "assert.scm")
 (include "parrot.scm")
 
-(test-case "Parrot" (list
-
-; it gets the speed of a european parrot
-(lambda ()
+(test-case "it gets the speed of an european parrot"
     (assert= 12.0
             (parrot-speed 'european-parrot 0 0.0 #f))) ; ')))
 
-; it gets the speed of an african parrot with one coconut
-(lambda ()
+(test-case "it gets the speed of an african parrot with one coconut"
     (assert= 3.0
             (parrot-speed 'african-parrot 1 0.0 #f))) ; ')))
 
-; it gets the speed of an african parrot with two coconuts
-(lambda ()
+(test-case "it gets the speed of an african parrot with two coconuts"
     (assert= 0.0
             (parrot-speed 'african-parrot 2 0.0 #f))) ; ')))
 
-; it gets the speed of an african parrot with no coconuts
-(lambda ()
+(test-case "it gets the speed of an african parrot with no coconuts"
     (assert= 12.0
             (parrot-speed 'african-parrot 0 0.0 #f))) ; ')))
 
-; it gets the speed of a nailed norwegian blue parrot
-(lambda ()
+(test-case "it gets the speed of a nailed norwegian blue parrot"
     (assert= 0.0
              (parrot-speed 'norwegian-blue-parrot 0 1.5 #t))) ; ')))
 
-; it gets the speed of a not nailed norwegian blue parrot
-(lambda ()
+(test-case "it gets the speed of a not nailed norwegian blue parrot"
     (assert= 18.0
              (parrot-speed 'norwegian-blue-parrot 0 1.5 #f))) ; ')))
 
-; it gets the speed of a not nailed norwegian blue parrot with high voltage
-(lambda ()
+(test-case "it gets the speed of a not nailed norwegian blue parrot with high voltage"
     (assert= 24.0
              (parrot-speed 'norwegian-blue-parrot 0 4.0 #f))) ; ')))
 
-; it can't get speed of unknown parrrot types
-(lambda ()
+(test-case "it can't get speed of unknown parrrot types"
     (assert-raise
         'Should-be-unreachable ; '
         (lambda () (parrot-speed 'unknown-parrot 0 0.0 #f)))) ; '))))
-
-))
