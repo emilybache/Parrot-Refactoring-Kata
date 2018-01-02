237420e8fcfb4bc84ff88da2de750cc7a93e33f5
Peter Kofler <peter.kofler@code-cop.org>
2018-01-02 14:09:32 +0100

    Adds lein-midje plugin to project for older lein versions.

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -1,5 +1,6 @@
 (defproject parrot-refactoring "0.0.1-SNAPSHOT"
   :description "Parrot Refactoring Kata"
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[midje "1.8.3"]]}
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2.1"]]}
              :midje {}})
