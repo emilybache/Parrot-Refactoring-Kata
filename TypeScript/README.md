485eb1646e61405bfa5de64cd5f849c79619502e
Peter Kofler <peter.kofler@code-cop.org>
2018-12-09 20:13:08 +0100

    Port to TypeScript

---
+++ TypeScript/README.md
@@ -0,0 +1,16 @@
+Parrot Refactoring Kata TypeScript version
+==========================================
+
+Parrot Refactoring Kata.
+
+This is a [npm](https://www.npmjs.com/) project.
+
+To install dependencies:
+
+    npm install
+
+The project uses [Mocha](https://mochajs.org/) and [Chai](https://www.chaijs.com/).
+
+To run the test:
+
+    npm test
---
+++ TypeScript/test/parrot.spec.ts
@@ -0,0 +1,48 @@
+import { expect } from "chai";
+import "mocha";
+import { Parrot, ParrotTypes } from "../src/parrot";
+
+describe("Parrot", () => {
+
+    it("gets speed of European Parrot", () => {
+        const parrot = new Parrot(ParrotTypes.EUROPEAN, 0, 0, false);
+
+        expect(parrot.getSpeed()).eql(12);
+    });
+
+    it("gets speed of African Parrot with one coconut", () => {
+        const parrot = new Parrot(ParrotTypes.AFRICAN, 1, 0, false);
+
+        expect(parrot.getSpeed()).eql(3);
+    });
+
+    it("gets speed of African Parrot with two coconuts", () => {
+        const parrot = new Parrot(ParrotTypes.AFRICAN, 2, 0, false);
+
+        expect(parrot.getSpeed()).eql(0);
+    });
+
+    it("gets speed of African Parrot with no coconuts", () => {
+        const parrot = new Parrot(ParrotTypes.AFRICAN, 0, 0, false);
+
+        expect(parrot.getSpeed()).eql(12);
+    });
+
+    it("gets speed of Norwegian Blue Parrot nailed", () => {
+        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, true);
+
+        expect(parrot.getSpeed()).eql(0);
+    });
+
+    it("gets speed of Norwegian Blue Parrot not nailed", () => {
+        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, false);
+
+        expect(parrot.getSpeed()).eql(18);
+    });
+
+    it("gets speed of Norwegian Blue Parrot not nailed high voltage", () => {
+        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
+
+        expect(parrot.getSpeed()).eql(24);
+    });
+});
