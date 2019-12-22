fdbd480bf17841bcbf3743418b1e3d0e497e96af
Pen-y-Fan <{ID}+{username}@users.noreply.github.com>
2019-12-22 12:15:08 +0000

    Updated to PHPUnit 8.5 / PHP 7.2
    Added PCH_CodeSniffer and PSR-12 coding standard.
    Code has only been changed to PSR-12 standard.
    Added composer scripts to run tests, checkcode and fixcode
    Updated the README to allow the Kata to be created and started quickly.

--- PHP/src/Parrot.php
+++ PHP/src/Parrot.php
@@ -1,45 +1,61 @@
 <?php
+
 namespace Parrot;
 
-class Parrot {
-  /** @var int ParrotTypeEnum */
-  private $type;
-  /** @var int */
-  private $numberOfCoconuts = 0;
-  /** @var double */
-  private $voltage;
-  /** @var boolean */
-  private $isNailed;
+use Exception;
 
-  public function __construct($type, $numberOfCoconuts, $voltage, $isNailed) {
-    $this->type = $type;
-    $this->numberOfCoconuts = $numberOfCoconuts;
-    $this->voltage = $voltage;
-    $this->isNailed = $isNailed;
-  }
+class Parrot
+{
+    /**
+     * @var int ParrotTypeEnum
+     */
+    private $type;
+    /**
+     * @var int
+     */
+    private $numberOfCoconuts;
+    /**
+     * @var float
+     */
+    private $voltage;
+    /**
+     * @var bool
+     */
+    private $isNailed;
 
-  public function getSpeed() {
-    switch ($this->type) {
-      case ParrotTypeEnum::EUROPEAN:
-        return $this->getBaseSpeed();
-      case ParrotTypeEnum::AFRICAN:
-        return max(0, $this->getBaseSpeed() - $this->getLoadFactor() * $this->numberOfCoconuts);
-      case ParrotTypeEnum::NORWEGIAN_BLUE:
-        return $this->isNailed ? 0 : $this->getBaseSpeedWith($this->voltage);
+    public function __construct($type, $numberOfCoconuts, $voltage, $isNailed)
+    {
+        $this->type = $type;
+        $this->numberOfCoconuts = $numberOfCoconuts;
+        $this->voltage = $voltage;
+        $this->isNailed = $isNailed;
     }
-    throw new \Exception("Should be unreachable");
-  }
 
-  private function getBaseSpeedWith($voltage) {
-    return min(24.0, $voltage * $this->getBaseSpeed());
-  }
+    public function getSpeed()
+    {
+        switch ($this->type) {
+            case ParrotTypeEnum::EUROPEAN:
+                return $this->getBaseSpeed();
+            case ParrotTypeEnum::AFRICAN:
+                return max(0, $this->getBaseSpeed() - $this->getLoadFactor() * $this->numberOfCoconuts);
+            case ParrotTypeEnum::NORWEGIAN_BLUE:
+                return $this->isNailed ? 0 : $this->getBaseSpeedWith($this->voltage);
+        }
+        throw new Exception("Should be unreachable");
+    }
 
-  private function getLoadFactor() {
-    return 9.0;
-  }
+    private function getBaseSpeedWith($voltage)
+    {
+        return min(24.0, $voltage * $this->getBaseSpeed());
+    }
 
-  private function getBaseSpeed() {
-    return 12.0;
-  }
+    private function getLoadFactor()
+    {
+        return 9.0;
+    }
 
+    private function getBaseSpeed()
+    {
+        return 12.0;
+    }
 }
