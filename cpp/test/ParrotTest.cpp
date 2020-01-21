68f60912c5421272d94bc9b71c10a022406bf6af
Emily Bache <emily.bache@praqma.com>
2020-01-21 13:51:39 +0100

    have tests delete test data

--- cpp/test/ParrotTest.cpp
+++ cpp/test/ParrotTest.cpp
@@ -4,36 +4,43 @@
 #include "../model/Parrot.h"
 
 TEST(ParrotTest, SpeedOfEuropeanParrot) {
-    auto parrot = new Parrot(EUROPEAN, 0, 0, false);
+    auto *parrot = new Parrot(EUROPEAN, 0, 0, false);
     EXPECT_NEAR(12.0, parrot->getSpeed(), 0.0);
+    delete parrot;
 }
 
 TEST(ParrotTest, SpeedOfAfricanParrot_With_One_Coconut) {
-    auto parrot = new Parrot(AFRICAN, 1, 0, false);
+    auto *parrot = new Parrot(AFRICAN, 1, 0, false);
     EXPECT_NEAR(3.0, parrot->getSpeed(), 0.0);
+    delete parrot;
 }
 
 TEST(ParrotTest, SpeedOfAfricanParrot_With_Two_Coconuts) {
-    auto parrot = new Parrot(AFRICAN, 2, 0, false);
+    auto *parrot = new Parrot(AFRICAN, 2, 0, false);
     EXPECT_NEAR(0.0, parrot->getSpeed(), 0.0);
+    delete parrot;
 }
 
 TEST(ParrotTest, SpeedOfAfricanParrot_With_No_Coconuts) {
-    auto parrot = new Parrot(AFRICAN, 0, 0, false);
+    auto *parrot = new Parrot(AFRICAN, 0, 0, false);
     EXPECT_NEAR(12.0, parrot->getSpeed(), 0.0);
+    delete parrot;
 }
 
 TEST(ParrotTest, SpeedNorwegianBlueParrot_nailed) {
-    auto parrot = new Parrot(NORWEGIAN_BLUE, 0, 1.5, true);
+    auto *parrot = new Parrot(NORWEGIAN_BLUE, 0, 1.5, true);
     EXPECT_NEAR(0.0, parrot->getSpeed(), 0.0);
+    delete parrot;
 }
 
 TEST(ParrotTest, SpeedNorwegianBlueParrot_not_nailed) {
-    auto parrot = new Parrot(NORWEGIAN_BLUE, 0, 1.5, false);
+    auto *parrot = new Parrot(NORWEGIAN_BLUE, 0, 1.5, false);
     EXPECT_NEAR(18.0, parrot->getSpeed(), 0.0);
+    delete parrot;
 }
 
 TEST(ParrotTest, SpeedNorwegianBlueParrot_not_nailed_high_voltage) {
-    auto parrot = new Parrot(NORWEGIAN_BLUE, 0, 4, false);
+    auto *parrot = new Parrot(NORWEGIAN_BLUE, 0, 4, false);
     EXPECT_NEAR(24.0, parrot->getSpeed(), 0.0);
+    delete parrot;
 }
