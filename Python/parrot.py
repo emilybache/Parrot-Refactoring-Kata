5ccd91796fccbb78bc6ede7210ca2019c706697d
Robert Roskam <raiderrobert@gmail.com>
2020-10-30 08:47:28 -0400

    my start

--- Python/parrot.py
+++ Python/parrot.py
@@ -1,38 +1,47 @@
-from enum import Enum  # Enum is introduced in Python 3.4.
+from enum import Enum
+from dataclasses import dataclass
+from typing import Union
 
 
-class ParrotType(Enum):  # If it is not available, just remove it.
+class ParrotType(Enum):
     EUROPEAN = 1
     AFRICAN = 2
     NORWEGIAN_BLUE = 3
 
 
-class Parrot:
+def _base_speed(self):
+    return 12.0
 
-    def __init__(self, type_of_parrot, number_of_coconuts, voltage, nailed):
-        self._type = type_of_parrot
-        self._number_of_coconuts = number_of_coconuts
-        self._voltage = voltage
-        self._nailed = nailed
+parrot_func_dict = {
+    ParrotType.EUROPEAN: _base_speed,
+    ParrotType.AFRICAN: ,
+    ParrotType.NORWEGIAN_BLUE:
+}
+
+@dataclass
+class Parrot:
+    type_of_parrot: ParrotType
+    number_of_coconuts: int
+    voltage: Union[int, float]
+    nailed: bool
 
     def speed(self):
-        if self._type == ParrotType.EUROPEAN:
+        if self.type_of_parrot == ParrotType.EUROPEAN:
             return self._base_speed()
-        if self._type == ParrotType.AFRICAN:
-            return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
-        if self._type == ParrotType.NORWEGIAN_BLUE:
-            if self._nailed:
+        if self.type_of_parrot == ParrotType.AFRICAN:
+            return max(0, self._base_speed() - self._load_factor() * self.number_of_coconuts)
+        if self.type_of_parrot == ParrotType.NORWEGIAN_BLUE: 
+            if self.nailed:
                 return 0
             else:
-                return self._compute_base_speed_for_voltage(self._voltage)
+                return self._compute_base_speed_for_voltage(self.voltage)
 
-        raise ValueError("should be unreachable")
+    raise ValueError("should be unreachable")
 
-    def _compute_base_speed_for_voltage(self, voltage):
-        return min([24.0, voltage * self._base_speed()])
+def _compute_base_speed_for_voltage(self, voltage):
+    return min([24.0, voltage * self._base_speed()])
 
-    def _load_factor(self):
-        return 9.0
+def _load_factor(self):
+    return 9.0
 
-    def _base_speed(self):
-        return 12.0
+
