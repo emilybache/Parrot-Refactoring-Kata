f60e4bbac1eff8472fb6a1e6985906a641bc8e47
rea <reasu@pm.me>
2021-12-18 23:55:45 +0100

    add Package.swift

---
+++ Swift/Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+ 13 
+ 12 import PackageDescription
+ 11 
+ 10 let package = Package(
+  9     name: "Parrot",
+  8     products: [
+  7     ],
+  6     targets: [
+  5         .testTarget(
+  4             name: "ParrotTests",
+  3             dependencies: [],
+  2             path: ""
+  1         ),
+  0     ]
+  1 )
