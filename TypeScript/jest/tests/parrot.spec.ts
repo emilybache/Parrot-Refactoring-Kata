21f8cc6ed92107c59cbc1f7af4eebc1b781b71c7
rea <reasu@pm.me>
2021-12-20 19:07:43 +0100

    fix indentation

--- TypeScript/jest/tests/parrot.spec.ts
+++ TypeScript/jest/tests/parrot.spec.ts
@@ -1,40 +1,40 @@
-import { Parrot, ParrotTypes } from "../src/parrot"
+import {Parrot, ParrotTypes} from '../src/parrot'
 
 describe('Parrot', () => {
 
-  it('gets speed of European Parrot', () => {
-    const parrot = new Parrot(ParrotTypes.EUROPEAN, 0, 0, false);
-    expect(parrot.getSpeed()).toBe(12);
-  });
+    it('gets speed of European Parrot', () => {
+        const parrot = new Parrot(ParrotTypes.EUROPEAN, 0, 0, false);
+        expect(parrot.getSpeed()).toBe(12);
+    });
 
-  it('gets speed of African Parrot with one coconut', () => {
-    const parrot = new Parrot(ParrotTypes.AFRICAN, 1, 0, false);
-    expect(parrot.getSpeed()).toBe(3);
-  });
+    it('gets speed of African Parrot with one coconut', () => {
+        const parrot = new Parrot(ParrotTypes.AFRICAN, 1, 0, false);
+        expect(parrot.getSpeed()).toBe(3);
+    });
 
-  it('gets speed of African Parrot with two coconuts', () => {
-    const parrot = new Parrot(ParrotTypes.AFRICAN, 2, 0, false);
-    expect(parrot.getSpeed()).toBe(0);
-  });
+    it('gets speed of African Parrot with two coconuts', () => {
+        const parrot = new Parrot(ParrotTypes.AFRICAN, 2, 0, false);
+        expect(parrot.getSpeed()).toBe(0);
+    });
 
-  it('gets speed of African Parrot with no coconuts', () => {
-    const parrot = new Parrot(ParrotTypes.AFRICAN, 0, 0, false);
-    expect(parrot.getSpeed()).toBe(12);
-  });
+    it('gets speed of African Parrot with no coconuts', () => {
+        const parrot = new Parrot(ParrotTypes.AFRICAN, 0, 0, false);
+        expect(parrot.getSpeed()).toBe(12);
+    });
 
-  it('gets speed of Norwegian Blue Parrot nailed', () => {
-    const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, true);
-    expect(parrot.getSpeed()).toBe(0);
-  });
+    it('gets speed of Norwegian Blue Parrot nailed', () => {
+        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, true);
+        expect(parrot.getSpeed()).toBe(0);
+    });
 
-  it('gets speed of Norwegian Blue Parrot not nailed', () => {
-    const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, false);
-    expect(parrot.getSpeed()).toBe(18);
-  });
+    it('gets speed of Norwegian Blue Parrot not nailed', () => {
+        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, false);
+        expect(parrot.getSpeed()).toBe(18);
+    });
 
-  it('gets speed of Norwegian Blue Parrot not nailed high voltage', () => {
-    const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
-    expect(parrot.getSpeed()).toBe(24);
-  });
+    it('gets speed of Norwegian Blue Parrot not nailed high voltage', () => {
+        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
+        expect(parrot.getSpeed()).toBe(24);
+    });
 
 });
