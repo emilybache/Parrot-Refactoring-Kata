06293456eedc6a3592e7d1be790856281905edaa
Jon Reid <jon@qualitycoding.org>
2022-06-10 11:19:28 -0700

    t - Declare all tests as throws
    This is now the standard way, because it lets you use standard helpers.

--- Swift/ParrotTests/ParrotTests.swift
+++ Swift/ParrotTests/ParrotTests.swift
@@ -1,37 +1,37 @@
 import XCTest
 
 class ParrotTests: XCTestCase {
-    func testSpeedOfEuropeanParrot() {
+    func testSpeedOfEuropeanParrot() throws {
         let parrot = Parrot(.european, numberOfCoconuts: 0, voltage: 0.0, isNailed: false)
         XCTAssertEqual(parrot.speed, 12.0)
     }
 
-    func testSpeedOfAfricanParrot_with_one_coconut() {
+    func testSpeedOfAfricanParrot_with_one_coconut() throws {
         let parrot = Parrot(.african, numberOfCoconuts: 1, voltage: 0.0, isNailed: false)
         XCTAssertEqual(parrot.speed, 3.0)
     }
 
-    func testSpeedOfAfricanParrot_with_two_coconuts() {
+    func testSpeedOfAfricanParrot_with_two_coconuts() throws {
         let parrot = Parrot(.african, numberOfCoconuts: 2, voltage: 0.0, isNailed: false)
         XCTAssertEqual(parrot.speed, 0.0)
     }
 
-    func testSpeedOfAfricanParrot_with_no_coconuts() {
+    func testSpeedOfAfricanParrot_with_no_coconuts() throws {
         let parrot = Parrot(.african, numberOfCoconuts: 0, voltage: 0.0, isNailed: false)
         XCTAssertEqual(parrot.speed, 12.0)
     }
 
-    func testSpeedOfNorwegianBlueParrot_nailed() {
+    func testSpeedOfNorwegianBlueParrot_nailed() throws {
         let parrot = Parrot(.norwegianBlue, numberOfCoconuts: 0, voltage: 0.0, isNailed: true)
         XCTAssertEqual(parrot.speed, 0.0)
     }
 
-    func testSpeedOfNorwegianBlueParrot_not_nailed() {
+    func testSpeedOfNorwegianBlueParrot_not_nailed() throws {
         let parrot = Parrot(.norwegianBlue, numberOfCoconuts: 0, voltage: 1.5, isNailed: false)
         XCTAssertEqual(parrot.speed, 18.0)
     }
 
-    func testSpeedOfNorwegianBlueParrot_not_nailed_high_voltage() {
+    func testSpeedOfNorwegianBlueParrot_not_nailed_high_voltage() throws {
         let parrot = Parrot(.norwegianBlue, numberOfCoconuts: 0, voltage: 4.0, isNailed: false)
         XCTAssertEqual(parrot.speed, 24.0)
     }
