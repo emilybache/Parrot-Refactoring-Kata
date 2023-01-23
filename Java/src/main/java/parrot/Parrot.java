5f38efcf7763b3414201c0bef404d0bae5a145bc
emily <emily@bacheconsulting.com>
2023-01-23 15:37:28 +0100

    sort out warnings for new java version

--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -2,10 +2,10 @@ package parrot;
 
 public class Parrot {
 
-    private ParrotTypeEnum type;
-    private int numberOfCoconuts;
-    private double voltage;
-    private boolean isNailed;
+    private final ParrotTypeEnum type;
+    private final int numberOfCoconuts;
+    private final double voltage;
+    private final boolean isNailed;
 
     public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         this.type = type;
@@ -15,16 +15,11 @@ public class Parrot {
     }
 
     public double getSpeed() {
-        switch (type) {
-            case EUROPEAN:
-                return getBaseSpeed();
-            case AFRICAN:
-                return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-            case NORWEGIAN_BLUE:
-                return (isNailed) ? 0 : getBaseSpeed(voltage);
-            default:
-                throw new RuntimeException("Should be unreachable");
-        }
+        return switch (type) {
+            case EUROPEAN -> getBaseSpeed();
+            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
+            case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
+        };
     }
 
     private double getBaseSpeed(double voltage) {
