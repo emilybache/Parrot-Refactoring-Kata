464a55a2b2bf4a20d7f0645ec35bee7f9ce15c13
emily <emily@bacheconsulting.com>
2023-06-06 10:59:16 +0200

    remove test fixture to simplify

--- c_catch2/test-catch2/sample_catch.cpp
+++ c_catch2/test-catch2/sample_catch.cpp
@@ -7,37 +7,54 @@ extern "C"
 }
 
 
-TEST_CASE ("Parrot") {
+TEST_CASE("European Parrot") {
     Parrot parrot;
-
-    SECTION("European Parrot") {
-        init_parrot(&parrot, EUROPEAN, 0, 0, false);
-        REQUIRE_THAT(get_speed(&parrot), Catch::Matchers::WithinAbs(12.0, 0.001));
-    }
-    SECTION("African Parrot one coconut") {
-        init_parrot(&parrot, AFRICAN, 1, 0, false);
-        REQUIRE_THAT(get_speed(&parrot), Catch::Matchers::WithinAbs(3.0, 0.001));
-    }
-    SECTION("African Parrot two coconut") {
-        init_parrot(&parrot, AFRICAN, 2, 0, false);
-        REQUIRE_THAT(get_speed(&parrot), Catch::Matchers::WithinAbs(0.0, 0.001));
-    }
-    SECTION("African Parrot no coconut") {
-        init_parrot(&parrot, AFRICAN, 0, 0, false);
-        REQUIRE_THAT(get_speed(&parrot), Catch::Matchers::WithinAbs(12.0, 0.001));
-    }
-    SECTION("Norwegian Blue Nailed") {
-        init_parrot(&parrot, NORWEGIAN_BLUE, 0, 1.5, true);
-        REQUIRE_THAT(get_speed(&parrot), Catch::Matchers::WithinAbs(0.0, 0.001));
-    }
-    SECTION("Norwegian Blue Not Nailed") {
-        init_parrot(&parrot, NORWEGIAN_BLUE, 0, 1.5, false);
-        REQUIRE_THAT(get_speed(&parrot), Catch::Matchers::WithinAbs(18.0, 0.001));
-    }
-    SECTION("Norwegian Blue Not Nailed High Voltage") {
-        init_parrot(&parrot, NORWEGIAN_BLUE, 0, 4, false);
-        REQUIRE_THAT(get_speed(&parrot), Catch::Matchers::WithinAbs(24.0, 0.001));
-    }
+    init_parrot(&parrot, EUROPEAN, 0, 0, false);
+    float speed = get_speed(&parrot);
+    REQUIRE_THAT(speed, Catch::Matchers::WithinAbs(12.0, 0.001));
 }
+
+TEST_CASE("African Parrot one coconut") {
+    Parrot parrot;
+    init_parrot(&parrot, AFRICAN, 1, 0, false);
+    float speed = get_speed(&parrot);
+    REQUIRE_THAT(speed, Catch::Matchers::WithinAbs(3.0, 0.001));
+}
+
+TEST_CASE("African Parrot two coconut") {
+    Parrot parrot;
+    init_parrot(&parrot, AFRICAN, 2, 0, false);
+    float speed = get_speed(&parrot);
+    REQUIRE_THAT(speed, Catch::Matchers::WithinAbs(0.0, 0.001));
+}
+
+TEST_CASE("African Parrot no coconut") {
+    Parrot parrot;
+    init_parrot(&parrot, AFRICAN, 0, 0, false);
+    float speed = get_speed(&parrot);
+    REQUIRE_THAT(speed, Catch::Matchers::WithinAbs(12.0, 0.001));
+}
+
+TEST_CASE("Norwegian Blue Nailed") {
+    Parrot parrot;
+    init_parrot(&parrot, NORWEGIAN_BLUE, 0, 1.5, true);
+    float speed = get_speed(&parrot);
+    REQUIRE_THAT(speed, Catch::Matchers::WithinAbs(0.0, 0.001));
+}
+
+TEST_CASE("Norwegian Blue Not Nailed") {
+    Parrot parrot;
+    init_parrot(&parrot, NORWEGIAN_BLUE, 0, 1.5, false);
+    float speed = get_speed(&parrot);
+    REQUIRE_THAT(speed, Catch::Matchers::WithinAbs(18.0, 0.001));
+}
+
+TEST_CASE("Norwegian Blue Not Nailed High Voltage") {
+    Parrot parrot;
+    init_parrot(&parrot, NORWEGIAN_BLUE, 0, 4, false);
+    float speed = get_speed(&parrot);
+    REQUIRE_THAT(speed, Catch::Matchers::WithinAbs(24.0, 0.001));
+}
+
 
 
