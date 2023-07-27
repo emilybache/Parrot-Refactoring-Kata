72d3af7290cab089c0b82c137bab751285e07d15
NicoSimoski <128238379+NicoSimoski@users.noreply.github.com>
2023-07-27 20:07:01 +0200

    Added implementation in common lisp
    - with system-definition (asdf)
    - with unit-tests (parachute)

---
+++ common-lisp/source/package.lisp
@@ -0,0 +1,13 @@
+;;;; package.lisp
+
+(defpackage :parrot
+  (:use :common-lisp)
+  (:export :parrot
+           :type
+           :number-of-coconuts
+	   :voltage
+	   :nailed
+           :european
+           :african
+	   :norwegian-blue
+	   :speed))
