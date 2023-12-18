fdb797ea88c45c3eb7dde1d5b794988f5356acdf
stephan <stephan@svoboda.or.at>
2023-12-18 22:55:51 +0100

    Extend TS Mocha-Chai by getCry

--- TypeScript/mocha-chai/test/parrot.spec.ts
+++ TypeScript/mocha-chai/test/parrot.spec.ts
@@ -45,4 +45,24 @@ describe("Parrot", () => {
 
         expect(parrot.getSpeed()).eql(24);
     });
+
+    it('gets cry of European Parrot', () => {
+        const parrot = new Parrot(ParrotTypes.EUROPEAN, 0, 0, false);
+        expect( parrot.getCry()).eql("Sqoork!");
+    });
+
+    it('gets cry of African Parrot', () => {
+        const parrot = new Parrot(ParrotTypes.AFRICAN, 1, 0, false);
+        expect( parrot.getCry()).eql("Sqaark!");
+    });
+    it('gets cry of Norwegian Blue with high voltage', () => {
+        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
+        expect( parrot.getCry()).eql("Bzzzzzz");
+    });
+
+    it('gets cry of NorwegianBlue without voltage', () => {
+        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 0, false);
+        expect(parrot.getCry()).eql("...");
+    });
+
 });
