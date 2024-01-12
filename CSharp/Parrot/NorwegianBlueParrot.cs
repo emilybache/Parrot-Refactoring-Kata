77ad3113296030ce12ba27559893135289c7dd0d
emily <emily@bacheconsulting.com>
2024-01-12 14:15:46 +0100

    r push down GetCry to subclasses

--- CSharp/Parrot/NorwegianBlueParrot.cs
+++ CSharp/Parrot/NorwegianBlueParrot.cs
@@ -13,4 +13,24 @@ public class NorwegianBlueParrot : Parrot
     {
         return _isNailed ? 0 : GetBaseSpeed(_voltage);
     }
+
+    public override string GetCry()
+    {
+        string value;
+        switch (_type)
+        {
+            case ParrotTypeEnum.EUROPEAN:
+                value = "Sqoork!";
+                break;
+            case ParrotTypeEnum.AFRICAN:
+                value = "Sqaark!";
+                break;
+            case ParrotTypeEnum.NORWEGIAN_BLUE:
+                value = _voltage > 0 ? "Bzzzzzz" : "...";
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        return value;
+    }
 }
--- CSharp/Parrot/Parrot.cs
+++ CSharp/Parrot/Parrot.cs
@@ -56,24 +56,6 @@ namespace Parrot
             return 12.0;
         }
 
-        public string GetCry()
-        {
-            string value;
-            switch (_type)
-            {
-                case ParrotTypeEnum.EUROPEAN:
-                    value = "Sqoork!";
-                    break;
-                case ParrotTypeEnum.AFRICAN:
-                    value = "Sqaark!";
-                    break;
-                case ParrotTypeEnum.NORWEGIAN_BLUE:
-                    value = _voltage > 0 ? "Bzzzzzz" : "...";
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-            return value;
-        }
+        public abstract string GetCry();
     }
 }
