2e65e1f00cbc9d86ef95b7d2110271149d313081
emily <emily@bacheconsulting.com>
2024-01-12 14:14:15 +0100

    r remove redundant code

--- CSharp/Parrot/AfricanParrot.cs
+++ CSharp/Parrot/AfricanParrot.cs
@@ -12,16 +12,6 @@ public class AfricanParrot : Parrot
 
     public override double GetSpeed()
     {
-        switch (_type)
-        {
-            case ParrotTypeEnum.EUROPEAN:
-                return GetBaseSpeed();
-            case ParrotTypeEnum.AFRICAN:
-                return Math.Max(0, GetBaseSpeed() - GetLoadFactor() * _numberOfCoconuts);
-            case ParrotTypeEnum.NORWEGIAN_BLUE:
-                return _isNailed ? 0 : GetBaseSpeed(_voltage);
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
+        return Math.Max(0, GetBaseSpeed() - GetLoadFactor() * _numberOfCoconuts);
     }
 }
