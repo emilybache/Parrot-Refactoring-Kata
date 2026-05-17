4f722130f94333f7d5b294488799b2dfc888a583
Andres Jaime <andrewjr@Andress-MacBook-Air.local>
2026-05-17 12:30:57 -0600

    test: use concrete subclasses instead of base Parrot class

--- Python/test_parrot.py
+++ Python/test_parrot.py
@@ -1,56 +1,56 @@
-from parrot import Parrot, ParrotType
+from parrot import Parrot, ParrotType, EuropeanParrot, AfricanParrot, NorwegianBlueParrot
 
 
 def test_speed_of_european_parrot():
-    parrot = Parrot(ParrotType.EUROPEAN, 0, 0, False)
+    parrot = EuropeanParrot()
     assert parrot.speed() == 12.0
 
 
 def test_cry_of_european_parrot():
-    parrot = Parrot(ParrotType.EUROPEAN, 0, 0, False)
+    parrot = EuropeanParrot()
     assert parrot.cry() == "Sqoork!"
 
 
 def test_speed_of_african_parrot_with_one_coconut():
-    parrot = Parrot(ParrotType.AFRICAN, 1, 0, False)
+    parrot = AfricanParrot(1)
     assert parrot.speed() == 3.0
 
 
 def test_cry_of_african_parrot():
-    parrot = Parrot(ParrotType.AFRICAN, 1, 0, False)
+    parrot = AfricanParrot(1)
     assert parrot.cry() == "Sqaark!"
 
 
 def test_speed_of_african_parrot_with_two_coconuts():
-    parrot = Parrot(ParrotType.AFRICAN, 2, 0, False)
+    parrot = AfricanParrot(2)
     assert parrot.speed() == 0.0
 
 
 def test_speed_of_african_parrot_with_no_coconuts():
-    parrot = Parrot(ParrotType.AFRICAN, 0, 0, False)
+    parrot = AfricanParrot(0)
     assert parrot.speed() == 12.0
 
 
 def test_speed_norwegian_blue_parrot_nailed():
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 1.5, True)
+    parrot = NorwegianBlueParrot(1.5, True)
     assert parrot.speed() == 0.0
 
 
 def test_speed_norwegian_blue_parrot_not_nailed():
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 1.5, False)
+    parrot = NorwegianBlueParrot(1.5, False)
     assert parrot.speed() == 18.0
 
 
 def test_speed_norwegian_blue_parrot_not_nailed_high_voltage():
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 4, False)
+    parrot = NorwegianBlueParrot(4, False)
     assert parrot.speed() == 24.0
 
 
 def test_cry_norwegian_blue_parrot_high_voltage():
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 4, False)
+    parrot = NorwegianBlueParrot(4, False)
     assert parrot.cry() == "Bzzzzzz"
 
 
 def test_cry_norwegian_blue_parrot_no_voltage():
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 0, False)
+    parrot = NorwegianBlueParrot(0, False)
     assert parrot.cry() == "..."
